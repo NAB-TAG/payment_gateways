d1ef9d41c03568fa593ab9d2d74600c6a92f3319
Nando Agustin Bravo <bravonandoagustin@gmail.com>
2024-01-02 16:33:54 -0300

    build: :tada: Empezando el proyecto

---
+++ src/app/layouts/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return(
+        <header>
+            <div>Animated Sections</div>
+            <div><a href="https://codepen.io/BrianCross/pen/PoWapLP">Original By Brian</a></div>
+        </header>
+    )
+}
